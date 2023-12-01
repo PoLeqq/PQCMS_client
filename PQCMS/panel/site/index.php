6d4f9bdbaf048895f97e7ff6875bd4960ecbd0d8
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-01 23:08:12 +0100

    Pierwsze pliki edytora

--- PQCMS/panel/site/index.php
+++ PQCMS/panel/site/index.php
@@ -6,4 +6,54 @@ if(empty($_SESSION["pqcms-panel-username"]))
     header("location: ../");
     die("Najpierw musisz się zalogować! Błędne przekierowanie.");
 }
-echo "Strona do edytowania";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PQCMS - Panel, Strona</title>
+
+<!--    <link rel="stylesheet" href="panel.css">-->
+    <link rel="stylesheet" href="site.css">
+    <link rel="icon" href="../../images/PQCMS.svg">
+</head>
+<body>
+    <nav>
+        <ul>
+            <?php
+                $site = json_decode(file_get_contents("site.json"),true);
+
+                foreach($site["tabs"] as $tab)
+                {
+                    echo<<<END
+                    <li title="${tab}">
+                        ${tab}
+                    </li>
+                    END;
+                }
+            ?>
+        </ul>
+    </nav>
+
+    <div id="mainFrame">
+        <iframe></iframe>
+    </div>
+
+    <script>
+
+        function sendMessage() {
+            const message = document.querySelector("#message").value;
+            const iframe = document.querySelector("iframe");
+            iframe.contentWindow.postMessage(message, "*");
+        }
+
+        let tabs = document.querySelectorAll("nav ul li");
+        tabs.forEach((e) => {
+            e.addEventListener("click", sendMessage);
+        })
+    </script>
+</body>
+</html>
