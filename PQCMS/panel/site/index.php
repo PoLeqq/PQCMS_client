175beb6384ef4179d9317225a417ce08198bedad
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-12 23:01:42 +0100

    Poprawki z generowaniem zakładek (stron), dodanie funkcjonalności zmiany strony

--- PQCMS/panel/site/index.php
+++ PQCMS/panel/site/index.php
@@ -29,7 +29,7 @@ if(empty($_SESSION["pqcms-panel-username"]))
                 foreach($site["tabs"] as $path => $name)
                 {
                     echo<<<END
-                    <li title="${path}">
+                    <li title="${name} - ${path}/" data-site="${path}">
                         ${name}
                     </li>
                     END;
@@ -39,20 +39,44 @@ if(empty($_SESSION["pqcms-panel-username"]))
     </nav>
 
     <div id="mainFrame">
-        <iframe src="editor/"></iframe>
+        <div id="mainIframeOverlay">
+            <img src="../images/preloader.gif" alt="preloader"/>
+        </div>
+        <iframe src="editor?site=_root_"></iframe>
     </div>
 
     <script>
+        const tabs = document.querySelectorAll("nav ul li");
+        const iframeOverlay = document.querySelector("#mainIframeOverlay");
+        const iframe = document.querySelector("#mainFrame iframe");
 
-        function sendMessage(element) {
-            const tab = element.getAttribute("title");
-            const iframe = document.querySelector("#mainFrame iframe");
-            iframe.src = "editor?site="+tab;
+        function changeIframeSrc(element)
+        {
+            const newUrl = element.getAttribute("data-site");
+            const currentURL = new URL(iframe.src);
+
+            const baseUrl = currentURL.origin + currentURL.pathname;
+            const pathURL = baseUrl + "?site=" + encodeURIComponent(newUrl);
+
+            if(currentURL.href === pathURL) return;
+
+            iframeOverlay.style.visibility = "visible";
+            iframeOverlay.style.opacity = "1";
+
+            iframe.src = pathURL;
         }
 
-        let tabs = document.querySelectorAll("nav ul li");
         tabs.forEach((e) => {
-            e.addEventListener("click", () => sendMessage(e));
+            e.addEventListener("click", () => changeIframeSrc(e));
+        })
+
+        iframe.addEventListener("load",() =>
+        {
+            iframeOverlay.style.opacity = "0";
+
+            setTimeout(() => {
+                iframeOverlay.style.visibility = "hidden";
+            },500);
         })
     </script>
 </body>
