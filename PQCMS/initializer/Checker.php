204ef0e1f5643e35d0bd51ef7feef70894c12024
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-16 23:58:46 +0100

    Poprawki

--- PQCMS/initializer/Checker.php
+++ PQCMS/initializer/Checker.php
@@ -3,7 +3,7 @@
 function isFirstTime(): ?bool
 {
     require_once(dirname(__DIR__) . "/Communicator.inc.php");
-    $adminExists = Communicator::communicate(CommunicateURL::DOES_ADMIN_EXISTS,[]);
+    $adminExists = Communicator::communicate(CommunicateURL::DOES_ADMIN_EXISTS);
     if($adminExists["suc"] == 0) return null;
     return !$adminExists["resp"];
 }
