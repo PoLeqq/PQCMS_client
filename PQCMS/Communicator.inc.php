185a7e78d8dd57531df3bda0692a6aec3042a621
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-16 23:59:23 +0100

    Poprawki communicator'a

--- PQCMS/Communicator.inc.php
+++ PQCMS/Communicator.inc.php
@@ -11,7 +11,7 @@ class Communicator
      * @param array $postData dane, które zostaną przesłane metodą POST. (podczas VERIFY_LICENSE przesłać pustą)
      * @return mixed|null zwraca return (json) z danego APIka (lub null, gdy połączenie nie powiedzie się)
      */
-    public static function communicate(string $path, array $postData): mixed
+    public static function communicate(string $path, array $postData = []): mixed
     {
         if($path == CommunicateURL::VERIFY_LICENSE)
         {
@@ -26,24 +26,35 @@ class Communicator
         }
         else
         {
-            $postData["domain"] = $_SERVER["SERVER_NAME"];
+            if(!in_array($path,CommunicateURL::getUnrequiredLoginSession()))
+            {
+                @session_start();
+                if(empty($_SESSION["pqcms-panel-auth_key"]))
+                    return json_encode(["suc" => 0, "desc" => "Akcja niemożliwa do spełnienia. Nie posiadasz aktywnej sesji!"],JSON_UNESCAPED_UNICODE);
+            }
+
+            if($_SERVER["SERVER_NAME"] === "localhost") $postData["domain"] = "localhost.localhost";
+            else $postData["domain"] = $_SERVER["SERVER_NAME"];
+
             $key = Communicator::communicate(CommunicateURL::VERIFY_LICENSE,["generate_secure_key" => true]);
             if($key["suc"] == 0)
-                return["suc" => 0, "desc" => "Bład podczas generowania klucza zabezpieczającego: ".$key["desc"]];
+                return["suc" => 0, "desc" => "Błąd podczas generowania klucza zabezpieczającego: ".$key["desc"]];
             $postData["secure_key"] = $key["secure_key"];
         }
 
         $options = array(
             'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n" .
+                    "Referer: https://${_SERVER["SERVER_NAME"]}\r\n",
                 'method'  => 'POST',
                 'content' => http_build_query($postData)
             )
         );
 
-        $targetUrl = 'http://localhost/pqcms/server/api/'.$path;
+        $targetUrl = 'https://poleq.pl/server/api/'.$path;
 
         $context = stream_context_create($options);
+
         $response = file_get_contents($targetUrl, false, $context);
 
 //        var_dump($response);
@@ -64,6 +75,15 @@ class CommunicateURL
     public const LOGIN_USER = "website/auth/LoginUser.php";
     public const LOGOUT_USER = "website/auth/LogoutUser.php";
     public const IS_VALID_AUTH_KEY = "website/auth/IsValidAuthKey.php";
+    public const HAS_PERMISSION = "website/perms/HasPermission.php";
     public const GET_SETTINGS = "website/settings/GetSettings.php";
     public const UPDATE_SETTINGS = "website/settings/UpdateSettings.php";
+
+    /**
+     * Funkcja zwracająca listę APIków, które dozwolone są do wykonania bez logowania do panelu
+     * @return array ścieżki APIków
+     */
+    public static function getUnrequiredLoginSession(): array {
+        return [self::VERIFY_LICENSE,self::DOES_ADMIN_EXISTS,self::LOGIN_USER];
+    }
 }
--- PQCMS/CommunicatorTest.php
+++ PQCMS/CommunicatorTest.php
@@ -17,9 +17,7 @@ require_once("Communicator.inc.php");
 //echo "\$client2\n";
 //print_r($client2);
 
-//f2758a8022409234a799b62d16b29790893afce46a41372189706d147e7775005562d32dfde795440618cc36e93b244668ab0e14c7e0ded877061181f79c0a6f
-//$client = Communicator::communicate(CommunicateURL::GET_CLIENT_VERSION,"fc3856cf0c53c9d3cd94f1bca15ce6054a482fea3e7dd14a102409221d0f94305589dcc54241931451f3ff44b3a5c70e758a4c002a9bbfd80c5450a51cdc49f2Array",[]);
-//$client = Communicator::communicate(CommunicateURL::GET_CLIENT_VERSION,$key["secure_key"],[]);
+//$client = Communicator::communicate(CommunicateURL::GET_CLIENT_VERSION,[]);
 //echo "\$client\n";
 //print_r($client);
 
@@ -37,10 +35,15 @@ require_once("Communicator.inc.php");
 //echo "\n\n";
 //$authKey = $loginUser["auth_key"];
 //
-//$validAuthKey = Communicator::communicate(CommunicateURL::IS_VALID_AUTH_KEY,["auth_key" => $authKey]);
-//echo "\$validAuthKey\n";
-//print_r($validAuthKey);
-//echo "\n\n";
+$validAuthKey = Communicator::communicate(CommunicateURL::IS_VALID_AUTH_KEY,["auth_key" => "b1c457af33c9faa944d9666bddafd0da27d273b6f5ee645b652aa2b308f122dfd0e1b73a34ac1c70fdcd26926fcb623aa4a41396c1d7a13ea3e3476ed0f32e91"]);
+echo "\$validAuthKey\n";
+print_r($validAuthKey);
+echo "\n\n";
+
+$hasPermission = Communicator::communicate(CommunicateURL::HAS_PERMISSION,["auth_key" => "12570c43b2b8281dfff0e51d376cabe1f7f93eaab57b41cc35806f0676afac3c54538a35c08ab5736fd9d8bd50cc82090913ee3dd3d89caa6143833a5ecdf367","perm" => "pqcms.test"]);
+echo "\$hasPermission\n";
+print_r($hasPermission);
+echo "\n\n";
 //
 //session_start();
 //$logoutUser = Communicator::communicate(CommunicateURL::LOGOUT_USER,["auth_key" => $authKey]);
@@ -63,6 +66,6 @@ require_once("Communicator.inc.php");
 // print_r($addUser);
 
 // $getUsers = Communicator::communicate(CommunicateURL::GET_USER,["username" => "user1234567", "nickname" => "user12345", "disabled" => false]);
-$getUsers = Communicator::communicate(CommunicateURL::GET_USER,[]);
-echo "\$getUsers\n";
-print_r($getUsers);
+//$getUsers = Communicator::communicate(CommunicateURL::GET_USER,[]);
+//echo "\$getUsers\n";
+//print_r($getUsers);
