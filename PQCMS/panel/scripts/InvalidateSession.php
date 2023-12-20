91aa07f987d8fe347037f0df07643c32e9c4d1c6
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-20 22:18:19 +0100

    Zwiększenie czytelności informacji (dla użytkownika, w sesji)

--- PQCMS/panel/scripts/InvalidateSession.php
+++ PQCMS/panel/scripts/InvalidateSession.php
@@ -7,21 +7,21 @@ if(empty($_SESSION["pqcms-panel-auth_key"]))
     die("Najpierw się zaloguj. Nieprawidłowe przekierowanie");
 }
 
-if((!isset($_GET["outdated"]) && !isset($_GET["invalidated"])))
-    die("Niepoprawne dane. Powodem może być niespójność między plikami lub błędne przekierowanie.");
+//if(!isset($_GET["outdated"]) && !isset($_GET["invalidated"]) && !isset($_GET["not_secure"]))
+//    die("Niepoprawne dane. Powodem może być niespójność między plikami lub błędne przekierowanie.");
 
-if(!isset($_GET["outdated"]) || !isset($_GET["invalidated"]))
-    die("Niepoprawne dane. Powodem może być niespójność między plikami lub błędne przekierowanie.");
-
-session_start();
+if($_GET["not_secure"] == 1)
+    $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona, ponieważ przesłane dane nie były bezpieczne!";
+else if($_GET["invalidated"] == 1)
+    $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona przez administratora!";
+else if($_GET["outdated"] == 1)
+    $_SESSION["pqcms-panel-login-error"] = "Twoja sesja przedawniła się!";
+else
+    $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona z nieznanego powodu!";
 
 foreach(array_keys($_SESSION) as $sessionKey)
     if(str_starts_with($sessionKey,"pqcms-"))
         unset($_SESSION[$sessionKey]);
-
-if($_GET["outdated"] == 1) $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona przez administratora!";
-else $_SESSION["pqcms-panel-login-error"] = "Twoja sesja wygasła!";
-//else if(!empty($_GET["invalidated"])) $_SESSION["pqcms-panel-login-error"] = "Twoja sesja wygasła!";
 
 header("location: ../");
 echo "Nieprawidłowe przekierowanie.";
