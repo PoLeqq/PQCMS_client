eecc7785e6ce31763ee31c108b31669b7b448294
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-29 23:57:53 +0100

    Poprawka - zwrócenie pustej tablicy, gdy sesja z powiadomieniami nie jest ustawiona

--- PQCMS/panel/scripts/notifications/GetNotifications.php
+++ PQCMS/panel/scripts/notifications/GetNotifications.php
@@ -1,3 +1,5 @@
 <?php
 @session_start();
+if(empty($_SESSION["pqcms"]["panel"]["notifications"]))
+    die(json_encode([]));
 echo json_encode($_SESSION["pqcms"]["panel"]["notifications"],JSON_UNESCAPED_UNICODE);
