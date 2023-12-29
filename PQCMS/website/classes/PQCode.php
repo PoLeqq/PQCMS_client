2cf62ded93de19ccdbbda75556040de38bc7249a
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-30 00:01:04 +0100

    Dodanie implementacji PQCode - skróty tłumaczone z kodu na html

--- PQCMS/website/classes/PQCode.php
+++ PQCMS/website/classes/PQCode.php
@@ -5,5 +5,39 @@
  */
 class PQCode
 {
-//    public static
+    protected $code;
+
+    public function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    private static array $codes = [
+        "[b]" => "<b>",
+        "[/b]" => "</b>",
+        "[i]" => "<i>",
+        "[/i]" => "</i>",
+        "[u]" => "<u>",
+        "[/u]" => "</u>",
+        "[s]" => "<s>",
+        "[/s]" => "</s>",
+//        "[link]" => "<a>",
+//        "[/link]" => "</a>",
+        "[br]" => "</br>",
+        "\n" => "</br>",
+        "[p]" => "<p>",
+        "[/p]" => "</p>",
+        "[quote]" => "<quote>",
+        "[/quote]" => "</quote>",
+    ];
+
+    public static function getCodes(): array
+    {
+        return self::$codes;
+    }
+
+    public function getHTML(): string
+    {
+        return strtr($this->code, self::$codes);
+    }
 }
