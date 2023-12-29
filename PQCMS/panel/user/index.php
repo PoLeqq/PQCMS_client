e2cec948cb7b643a03db20c7caf391732b460096
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-30 00:13:46 +0100

    Dodanie zakładki, która umożliwia przeglądanie danych usera (aktualnego)

---
+++ PQCMS/panel/user/index.php
@@ -0,0 +1,72 @@
+<?php
+
+@session_start();
+if(empty($_SESSION["pqcms-panel-auth_key"]))
+{
+    header("location: ../");
+    die("Najpierw musisz się zalogować! Błędne przekierowanie.");
+}
+
+require_once(dirname(__DIR__,2)."/Communicator.inc.php");
+$user = Communicator::communicate(CommunicateURL::GET_USER,["username" => $_SESSION["pqcms-panel-username"]])["resp"];
+var_dump($user);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PQCMS - Panel, Użytkownik</title>
+
+    <!--    <link rel="stylesheet" href="panel.css">-->
+    <link rel="stylesheet" href="user.css">
+    <link rel="icon" href="../../images/PQCMS.svg">
+</head>
+<body>
+
+<div id="mainFrame">
+    <h1>Twoje dane</h1>
+
+    <form method="post" action="ChangeUserData.php">
+        <input name="username" value="<?php ?>">
+    </form>
+</div>
+
+<script>
+    const tabs = document.querySelectorAll("nav ul li");
+    const iframeOverlay = document.querySelector("#mainIframeOverlay");
+    const iframe = document.querySelector("#mainFrame iframe");
+
+    function changeIframeSrc(element)
+    {
+        const newUrl = element.getAttribute("data-site");
+        const currentURL = new URL(iframe.src);
+
+        const baseUrl = currentURL.origin + currentURL.pathname;
+        const pathURL = baseUrl + "?site=" + encodeURIComponent(newUrl);
+
+        if(currentURL.href === pathURL) return;
+
+        iframeOverlay.style.visibility = "visible";
+        iframeOverlay.style.opacity = "1";
+
+        iframe.src = pathURL;
+    }
+
+    tabs.forEach((e) => {
+        e.addEventListener("click", () => changeIframeSrc(e));
+    })
+
+    iframe.addEventListener("load",() =>
+    {
+        iframeOverlay.style.opacity = "0";
+
+        setTimeout(() => {
+            iframeOverlay.style.visibility = "hidden";
+        },500);
+    })
+</script>
+</body>
+</html>
