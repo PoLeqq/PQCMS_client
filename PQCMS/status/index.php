343829527a61e6d51e4b81cbcc938f91047d544a
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-07 23:23:25 +0100

    Skrócenie kodu

--- PQCMS/status/index.php
+++ PQCMS/status/index.php
@@ -3,7 +3,7 @@
 header("Content-type: application/json; charset=utf-8");
 require_once(dirname(__DIR__)."/Communicator.inc.php");
 
-$verifyLicense = Communicator::communicate(CommunicateURL::VERIFY_LICENSE,[]);
+$verifyLicense = Communicator::communicate(CommunicateURL::VERIFY_LICENSE);
 unset($verifyLicense["expiry_date"]);
 
 print_r($verifyLicense);
