089245652bbf26682263882199def1b452168eb6
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-08 23:00:39 +0100

    Usunięcie sprawdzenia is_null do resp communicatora

--- PQCMS/initializer/Checker.php
+++ PQCMS/initializer/Checker.php
@@ -4,6 +4,6 @@ function isFirstTime(): ?bool
 {
     require_once(dirname(__DIR__) . "/Communicator.inc.php");
     $adminExists = Communicator::communicate(CommunicateURL::DOES_ADMIN_EXISTS);
-    if(is_null($adminExists) || $adminExists["suc"] == 0) return null;
+    if($adminExists["suc"] == 0) return null;
     return !$adminExists["resp"];
 }
