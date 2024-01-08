fbc3770dcf3b2b15eafb5d28ab893733aa79fbbf
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-08 23:03:48 +0100

    Naprawa braku pokazywania błędu po invalidacji usera

--- PQCMS/panel/scripts/InvalidateSession.php
+++ PQCMS/panel/scripts/InvalidateSession.php
@@ -10,6 +10,10 @@ if(empty($_SESSION["pqcms-panel-auth_key"]))
 //if(!isset($_GET["outdated"]) && !isset($_GET["invalidated"]) && !isset($_GET["not_secure"]))
 //    die("Niepoprawne dane. Powodem może być niespójność między plikami lub błędne przekierowanie.");
 
+foreach(array_keys($_SESSION) as $sessionKey)
+    if(str_starts_with($sessionKey,"pqcms-"))
+        unset($_SESSION[$sessionKey]);
+
 if($_GET["not_secure"] == 1)
     $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona, ponieważ przesłane dane nie były bezpieczne! (niepoprawny \"auth_key\")";
 else if($_GET["invalidated"] == 1)
@@ -18,10 +22,6 @@ else if($_GET["outdated"] == 1)
     $_SESSION["pqcms-panel-login-error"] = "Twoja sesja wygasła!";
 else
     $_SESSION["pqcms-panel-login-error"] = "Twoja sesja została unieważniona z nieznanego powodu!";
-
-foreach(array_keys($_SESSION) as $sessionKey)
-    if(str_starts_with($sessionKey,"pqcms-"))
-        unset($_SESSION[$sessionKey]);
 
 header("location: ../");
 echo "Nieprawidłowe przekierowanie.";
