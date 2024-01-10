cf67035b2551e5b1af58f1586f05c4befde628ac
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-10 23:29:46 +0100

    Tabela u

--- PQCMS/panel/hr/index.php
+++ PQCMS/panel/hr/index.php
@@ -1,11 +1,8 @@
 <?php
 
-@session_start();
-if(empty($_SESSION["pqcms-panel-username"]))
-{
-    header("location: ../");
-    die("Najpierw musisz się zalogować!");
-}
+// Sprawdzenie, czy user posiada permisje do strony
+require_once(dirname(__DIR__)."/scripts/server/TabUtils.inc.php");
+TabUtils::verifyUser("hr");
 
 ?>
 
@@ -27,41 +24,62 @@ if(empty($_SESSION["pqcms-panel-username"]))
         <iframe></iframe>
     </div>
     <!-- <i>Jeżeli chcesz ustawić pole na puste, po prostu nic nie wpisuj.</i> -->
-    <div class="d-flex" id="main">
-        <div class="col-2 disableSelect" id="hrNav">
-            <ul>
-                <li class="expand">
-                    <div class="liManageable">
-                        <div class="col-10 d-flex align-items-center">
-                            <div class="col-2 ulArrow" rotate="true">
-                                <img src="images/arrow.svg">
-                            </div>
-                            <div class="ulText">
-                                Użytkownicy
-                            </div>
-                        </div>
-                        <div class="col-2 ulPlus">
-                            <img class="overlayLink" overlayPath="./overlays/users/" src="images/plus.svg">
-                        </div>
-                    </div>
-                    <ul class="px-4 ulHideable">
-                        <?php
+    <div class="d-flex">
+        <div class="col-4 main-section">
+            <div class="d-flex">
+                <div class="col-10 d-flex align-items-center">
+                    <h3>
+                        Użytkownicy
+                    </h3>
+                </div>
+                <div class="col-2 addImage img-fluid">
+                    <img class="overlayLink" data-overlayPath="./overlays/adders/users/" src="images/plus.svg" alt="plus">
+                </div>
+            </div>
+            <i>L_Nazwa - nazwa do loginu</i><br/>
+            <i>W_Nazwa - wyświetlana nazwa</i><br/>
+            <i>Stan - czy konto jest włączone (można się zalogować)</i><br/>
+            <i>Sesja - czy sesja konta jest aktywna (użytkownik jest zalogowany)</i>
+            <table class="px-4 my-3 col-12 data-table" id="users-table">
+                <thead>
+                <tr>
+                    <th>L_Nazwa</th>
+                    <th>W_Nazwa</th>
+                    <th>Stan</th>
+                    <th>Sesja</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $usersResponse = Communicator::communicate(CommunicateURL::GET_USER,["admin" => 0]);
 
-                        //                        TODO Connector may change this code
-//                        require_once("../../hr/User.php");
-                        //                        foreach(getAllUsers("priority",false) as $user)
-                        //                            echo "<li>$user->nickname</li>";
+                if($usersResponse["suc"] == 1)
+                {
+                    foreach($usersResponse["resp"] as $user)
+                    {
+                        if($user["username"] === $_SESSION["pqcms-panel-auth_key"])
+                            continue;
+                        $enabledClass = $user["disabled"] ? "enabled-no" : "enabled-yes";
+                        $enabledText = $user["disabled"] ? "Wył" : "Wł";
+                        $sessionClass = isset($user["active_session"]) ? "enabled-yes" : "enabled-no";
+                        $sessionText = isset($user["active_session"]) ? "<abbr title=\"Sesja wygasa: ${user["active_session"]}\">Wł</abbr>" : "Wył";
 
-                        require_once(dirname(__DIR__,2)."/Communicator.inc.php");
-                        $users = Communicator::communicate(CommunicateURL::GET_USER,["admin" => 0])["resp"];
-
-                        foreach($users as $user)
-                        {
-                            echo<<<HTML
-<li>
-    <b>${user["nickname"]}</b>
-    (${user["username"]})
-</li>
+                        $urlencodedPerms = urlencode(json_encode($user["perms"]));
+                        echo<<<HTML
+<tr class="overlayLink" data-overlayPath="./overlays/editors/users/index.php?username=${user["username"]}&nickname=${user["nickname"]}&disabled=${user["disabled"]}&perms=${urlencodedPerms}">
+    <td>${user["username"]}</td>
+    <td>${user["nickname"]}</td>
+    <td class="${enabledClass}">${enabledText}</td>
+    <td class="${sessionClass} user-session-close" data-username="${user["username"]}">${sessionText}</td>
+</tr>
+HTML;
+                    }
+                }
+                else
+                    echo<<<HTML
+<span style="color: red">
+    Wystąpił błąd podczas pobierania użytkowników: ${$usersResponse["desc"]}
+</span>
 HTML;
                         }
 
