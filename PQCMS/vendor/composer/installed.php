5e297a4bae9e758c4b216c9ba33d5003846e49b3
PoLeqq <wiktorsolinski123@gmail.com>
2024-02-02 12:57:48 +0100

    + - pliki composer (BBCode)

---
+++ PQCMS/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/pqcmsclient',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '99574b5f23cf1afda36de54abc160fbed12e2e37',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'genert/bbcode' => array(
+            'pretty_version' => '1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => '8c52c54fe08cc1a38566c7a02d39bc8b3c9453ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../genert/bbcode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor_name/pqcmsclient' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '99574b5f23cf1afda36de54abc160fbed12e2e37',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
