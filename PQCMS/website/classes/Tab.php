f8638e772b6fbfd6d98318f0df1235e900566907
PoLeqq <wiktorsolinski123@gmail.com>
2024-02-18 23:03:57 +0100

    / duuużo poprawek xD

--- PQCMS/website/classes/Tab.php
+++ PQCMS/website/classes/Tab.php
@@ -4,29 +4,112 @@ abstract class Tab
 {
     protected string $name;
     protected string $path;
+    protected bool $editable;
+    protected string $longSourcePath;
     protected array $texts;
+    protected array $groups;
 
-    /**
-     * @param string $name
-     * @param string $path
-//     * @param array $texts
-     */
-    public function __construct(string $name, string $path, array $texts = [])
+    public function __construct(string $name, string $path, bool $editable, string $longSourcePath, array $texts = [], array $groups = [])
     {
         $this->name = $name;
         $this->path = $path;
+        $this->editable = $editable;
+        $this->longSourcePath = $longSourcePath;
+
         $this->texts = $texts;
         $this->texts[] = "*";
+
+        $this->groups = $groups;
+        $this->groups[] = "*";
+
+        if($editable)
+        {
+            $perms = [];
+            foreach ($this->texts as $text)
+                $perms[] = "pqcms.site.text.set.$text";
+            foreach ($this->groups as $group)
+                $perms[] = "pqcms.site.group.set.$group";
+
+            require_once(dirname(__DIR__, 2) . "/Communicator.inc.php");
+            $hasPermission = (Communicator::communicate(CommunicateURL::HAS_PERMISSION, ["perms" => $perms]));
+            $isPermissionSet = (Communicator::communicate(CommunicateURL::IS_PERMISSION_SET, ["perms" => $perms]));
+
+            if($hasPermission["suc"] == 0)
+            {
+                require_once(dirname(__DIR__,2)."/panel/scripts/notifications/NotificationManager.inc.php");
+                NotificationManager::addNewNotification("editor-permissions-communicator-error","Edytor","e",
+                    "Wystąpił błąd podczas komunikacji z serwerami PQCMS!");
+                die();
+            }
+            else if($isPermissionSet["suc"] == 0)
+            {
+                require_once(dirname(__DIR__,2)."/panel/scripts/notifications/NotificationManager.inc.php");
+                NotificationManager::addNewNotification("editor-permissions-communicator-error","Edytor","e",
+                    "Wystąpił błąd podczas komunikacji z serwerami PQCMS!");
+                die();
+            }
+
+            $hasPermission = $hasPermission["perms"];
+            $isPermissionSet = $isPermissionSet["perms"];
+
+            foreach ($this->texts as $text) {
+                $textObj = Text::getTextByName($text);
+                if(!is_null($textObj)) {
+//                    pqcms.site.text.set.<id> - if isset
+//                    pqcms.site.group.set.<id> - if isset
+//                    pqcms.site.group.set.* - if isset
+//                    pqcms.site.text.set.<id> - value
+                    if($isPermissionSet["pqcms.site.text.set.$text"])
+                        $this->texts[$text] = $textObj->generateHtml($text, true, !$hasPermission["pqcms.site.text.set.$text"]);
+                    else
+                    {
+                        $group = $textObj->getGroup();
+                        if(!is_null($group) && $group->doesExists())
+                        {
+                            if($isPermissionSet["pqcms.site.group.set.".$group->getName()])
+                                $this->texts[$text] = $textObj->generateHtml($text, true, !$hasPermission["pqcms.site.group.set.".$group->getName()]);
+                            else if($isPermissionSet["pqcms.site.group.set.*"])
+                                $this->texts[$text] = $textObj->generateHtml($text, true, !$hasPermission["pqcms.site.group.set.*"]);
+                            else
+                                $this->texts[$text] = $textObj->generateHtml($text, true, !$hasPermission["pqcms.site.text.set.*"]);
+                        }
+                        else
+                            $this->texts[$text] = $textObj->generateHtml($text, true, !$hasPermission["pqcms.site.text.set.*"]);
+                    }
+                }
+                else
+                    $this->texts[$text] = "{PQCMS:[-]}";
+            }
+        }
+        else
+        {
+            foreach($this->texts as $text) {
+                $textObj = Text::getTextByName($text);
+                if(!is_null($textObj))
+                    $this->texts[$text] = Text::getTextByName($text)->generateHtml($text, false);
+                else
+                    $this->texts[$text] = "";
+            }
+        }
+    }
+
+    protected function getSourcePath(): string
+    {
+        if($this->editable)
+            return $this->longSourcePath;
+        return "";
     }
 
 //    todo może do pliku i po prostu odczyt z pliku??
-    protected function generateJS($jsonTexts): string
+    protected function generateJS(array $jsonTexts): string
     {
+        $jsonTexts = json_encode($jsonTexts,JSON_UNESCAPED_UNICODE);
+
         return<<<JS
 <script>
     
     {
-        const texts = ${jsonTexts};
+        const texts = $jsonTexts;
         let changes = [];
         
         for(let key in texts)
@@ -99,5 +182,60 @@ abstract class Tab
 JS;
     }
 
-    public abstract function generateHtml(bool $editable): string;
+    public final function generateSiteCode(string $lang = "pl"): string
+    {
+        if($this->editable)
+        {
+            @session_start();
+            if(empty($_SESSION["pqcms"]["panel"]["auth_key"]))
+                return "Najpierw musisz się zalogować!";
+        }
+
+        $head = $this->generateHeadCode();
+        $body = $this->generateBodyCode();
+
+        $sourcePath = $this->getSourcePath();
+
+        $head .= <<<HTML
+<link rel="stylesheet" href="${sourcePath}pqcms/pqcms.css">
+<script src="${sourcePath}pqcms/pqcms.js" defer></script>
+HTML;
+
+        if($this->editable)
+        {
+            $head .= <<<HTML
+<link rel="stylesheet" href="overlay.css">
+HTML;
+
+            $body = <<<HTML
+<form method='post' action='ChangeTabText.php' id='pqcms-editor-form'>
+$body
+</form>
+HTML;
+            $body .= $this->generateJS($this->texts);
+        }
+
+        return<<<HTML
+<!DOCTYPE html>
+<html lang="$lang">
+<head>
+    $head
+</head>
+<body>
+    $body
+</body>
+</html>
+HTML;
+    }
+
+    public abstract function generateHeadCode(string $lang = "pl"): string;
+    public abstract function generateBodyCode(string $lang = "pl"): string;
+
+    public function saveVisit(string $remoteAddr): void
+    {
+        require_once(dirname(__DIR__)."/TabsCounter.inc.php");
+        $tabsCounter = new TabsCounter();
+        $tabsCounter->saveVisit($this->name,$remoteAddr);
+        $tabsCounter->close();
+    }
 }
