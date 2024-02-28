9e0aed78323ade49a4a62fe7b3726004eead0b0d
PoLeqq <wiktorsolinski123@gmail.com>
2024-02-28 22:49:24 +0100

    / od teraz zostaje zapisywana również lokalizacja usera

--- PQCMS/website/TabsCounter.inc.php
+++ PQCMS/website/TabsCounter.inc.php
@@ -12,12 +12,27 @@ class TabsCounter
 
     public function saveVisit(string $tabName, string $remoteAddr): void
     {
-        $stmt = $this->conn->prepare("INSERT INTO pqcms_tabs_counter (id, tab_name, ip, date) VALUES (null, ?, ?, ?)");
+        $continent = null;
+        $country = null;
+        $city = null;
+
+        $user_ip = getenv('REMOTE_ADDR');
+
+        $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+        if($geo !== false)
+        {
+            $continent = $geo["geoplugin_continentCode"];
+            $country = $geo["geoplugin_countryCode"];
+            $city = $geo["geoplugin_city"];
+        }
+
+        $stmt = $this->conn->prepare("INSERT INTO pqcms_tabs_counter VALUES (null, ?, ?, ?, ?, ?, ?)");
+        echo $this->conn->error;
 
         date_default_timezone_set("Europe/Warsaw");
         $date = date('Y-m-d H:i:s',time());
 
-        $stmt->bind_param("sss",$tabName,$remoteAddr,$date);
+        $stmt->bind_param("ssssss",$tabName,$remoteAddr,$date,$continent,$country,$city);
         $stmt->execute();
         $stmt->close();
     }
