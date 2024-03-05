a52abc6da8f594d07d48db8b168b787f59de4a2b
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-05 22:10:07 +0100

    + funkcjonalność klasy

--- PQCMS/addons/classes/AddonPanel.inc.php
+++ PQCMS/addons/classes/AddonPanel.inc.php
@@ -1,15 +1,38 @@
 <?php
 
+require_once("Addon.inc.php");
+
 /**
  * Reprezentuje panel dodatku
  */
 abstract class AddonPanel
 {
-    public function getWebsite()
+    /**
+     * @var Addon dodatek
+     */
+    protected Addon $addon;
+
+    /**
+     * @param Addon $addon dodatek
+     */
+    public function __construct(Addon $addon)
     {
-        @session_start();
-//        if($_SESSION[""])
+        $this->addon = $addon;
     }
 
-    protected abstract function getWebsiteHTML();
+    public function generateWebsite(): void
+    {
+        $id = $this->addon->getId();
+        $permName = "pqcms.addons.$id";
+
+        require_once(dirname(__DIR__,2)."/panel/scripts/server/TabUtils.inc.php");
+        $verify = TabUtils::verifyUser("addons",[$permName]);
+
+        if($verify["perms"][$permName] !== 1)
+            die("Nie masz uprawnień!");
+
+        echo $this->getWebsiteHTML();
+    }
+
+    protected abstract function getWebsiteHTML(): string;
 }
