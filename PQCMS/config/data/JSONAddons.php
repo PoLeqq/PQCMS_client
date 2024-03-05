763fed46ff555ff8554dcb660d39b1a23d5375b2
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-05 22:11:20 +0100

    / modyfikacja klasy pod addony

--- PQCMS/config/data/JSONAddons.php
+++ PQCMS/config/data/JSONAddons.php
@@ -13,34 +13,10 @@ class JSONAddons extends JSONObject
     }
 
     /**
-     * Zwraca listę formularzy
+     * Zwraca listę ID włączonych dodatków
      */
-    public function getForms(): array
+    public function getAddons(): array
     {
-        $forms = [];
-        foreach ($this->data as $form)
-            $forms[] = new Form($form["id"],$form["name"],$form["enabled"],$form["cols"]);
-        return $forms;
-    }
-
-    /**
-     * Zwraca formularz o konkretnej nazwie
-     */
-    public function getForm(string $id): ?Form
-    {
-        foreach($this->data as $form)
-            if($form["id"] === $id)
-                return new Form($form["id"], $form["name"], $form["enabled"], $form["cols"]);
-
-        return null;
-    }
-
-    public function doesFormExists(string $name): bool
-    {
-        foreach($this->getForms() as $form)
-            if($form["name"] === $name)
-                return true;
-
-        return false;
+        return $this->data;
     }
 }
