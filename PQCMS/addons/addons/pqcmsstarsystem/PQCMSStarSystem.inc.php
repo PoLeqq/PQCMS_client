b0d0a73877913d5b5ef13bc6f65b42c09e2cc40a
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-05 22:08:21 +0100

    + przykładowy addon

--- PQCMS/addons/addons/pqcmsstarsystem/PQCMSStarSystem.inc.php
+++ PQCMS/addons/addons/pqcmsstarsystem/PQCMSStarSystem.inc.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once(dirname(__DIR__,2)."/classes/Addon.inc.php");
-require_once("PQCMSStarSystemPanel.inc.php");
+require_once("panel/PQCMSStarSystemPanel.inc.php");
 class PQCMSStarSystem extends Addon
 {
+    public const addonID = "pqcmsstarsystem";
     public function __construct()
     {
-        parent::__construct("pqcmsstarsystem", new PQCMSStarSystemPanel());
+        parent::__construct(PQCMSStarSystem::addonID, new PQCMSStarSystemPanel($this));
     }
 }
