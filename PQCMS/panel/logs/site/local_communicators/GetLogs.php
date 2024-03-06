a470d383b7c750f153e75bf35deb5f769eb20c8c
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-06 22:12:43 +0100

    / update kodu

--- PQCMS/panel/logs/site/local_communicators/GetLogs.php
+++ PQCMS/panel/logs/site/local_communicators/GetLogs.php
@@ -5,11 +5,15 @@ header("Content-Type: application/json");
 $post = json_decode(file_get_contents("php://input"), true);
 if($post === null)
     die(json_encode(["suc" => 0, "desc" => "Invalid POST data"]));
+
+$posts = [];
 if(empty($post["type"]))
     die(json_encode(["suc" => 0, "desc" => "You need to post \"type\"!"]));
-if(isset($post["type"]))
-    die(json_encode(["suc" => 0, "desc" => "You need to post \"type\"!"]));
-
+else $posts["type"] = $post["type"];
+if(isset($post["from"]))
+    $posts["from"] = $post["from"];
+if(isset($post["amount"]))
+    $posts["amount"] = $post["amount"];
 
 require_once(dirname(__DIR__,4)."/Communicator.inc.php");
 die(json_encode((Communicator::communicate(CommunicateURL::GET_LOGS,$post)),JSON_UNESCAPED_UNICODE));
