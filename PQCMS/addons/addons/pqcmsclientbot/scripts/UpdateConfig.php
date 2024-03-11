c56bff4f476fec4a81c937c853418a66efc11f99
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-11 23:44:17 +0100

    + skrypt edytujący config

--- PQCMS/addons/addons/pqcmsclientbot/scripts/UpdateConfig.php
+++ PQCMS/addons/addons/pqcmsclientbot/scripts/UpdateConfig.php
@@ -1,40 +1,21 @@
 <?php
 
+//todo PQCMSToken
+$botName = &$_POST["botname"];
+$errorMessage = &$_POST["errormessage"];
+$startMessages = &$_POST["startmessages"];
 
-$starRedirectRate = &$_POST["star-redirect-rate"];
-$redirectURL = &$_POST["redirect-url"];
-$redirectNewPage = &$_POST["redirect-new-page"];
+if(!isset($botName) || !isset($errorMessage) || !isset($startMessages))
+    die("Niepoprawny formularz!");
 
-if(empty($starRedirectRate))
-    die("Liczba gwiazdek jest wymagana!");
-else
-{
-    $starRedirectRate = filter_var($starRedirectRate, FILTER_VALIDATE_INT);
-    if(is_null($starRedirectRate))
-        die("Liczba gwiazdek musi być liczbą!");
-    if($starRedirectRate < 1 || $starRedirectRate > 5)
-        die("Liczba gwiazdek musi być z przedziału 1-5!");
-}
 
-if(empty($redirectURL) || !filter_var($redirectURL, FILTER_VALIDATE_URL))
-    die("Link jest niepoprawny!");
-
-if(!isset($redirectNewPage))
-    die("Opcja \"nowe okno\" jest wymagana!");
-else
-{
-    $redirectNewPage = filter_var($redirectNewPage, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
-    if(is_null($redirectNewPage))
-        die("Opcja \"nowe okno\" powinna być wartością prawda/fałsz!");
-}
-
-require(dirname(__DIR__)."/PQCMSStarSystem.inc.php");
-$addon = new PQCMSStarSystem();
-$addon->setConfig([
-    "star-redirect-rate" => $starRedirectRate,
-    "redirect-url" => $redirectURL,
-    "redirect-new-page" => $redirectNewPage
-    ]);
+require(dirname(__DIR__)."/PQCMSClientBot.inc.php");
+$addon = new PQCMSClientBot();
+$cfg = $addon->getConfig();
+$cfg["bot_name"] = $botName;
+$cfg["error_message"] = $errorMessage;
+$cfg["start_messages"] = $startMessages;
+$addon->setConfig($cfg);
 
 $path = $addon->getRelativePathFromAddonToPanel();
 header("location: ../$path");
