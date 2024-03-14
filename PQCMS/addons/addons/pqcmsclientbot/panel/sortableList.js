09952687a5e15f1dd99e45af89334287b451a5bb
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-14 23:08:31 +0100

    + skrypt obsługujący domyślne wiadomości oraz wiadomości bota

--- PQCMS/addons/addons/pqcmsclientbot/panel/sortableList.js
+++ PQCMS/addons/addons/pqcmsclientbot/panel/sortableList.js
@@ -4,8 +4,6 @@ new Sortable(startMessages, {
     animation: 150,
     ghostClass: 'blue-background-class',
     onChange: function () {
-        console.log(startMessages);
-        console.log(startMessages.children);
         let x = 0;
         startMessages.querySelectorAll(".text-inputs").forEach(inputDiv => {
             inputDiv.querySelectorAll("input[type=text]").forEach(input => {
@@ -13,5 +11,44 @@ new Sortable(startMessages, {
             })
             x++;
         })
+    },
+    onStart: function(event) {
+        // console.log(event.originalEvent.target);
+        event.item.style.filter = "brightness(2) blur(1px)";
+    },
+
+    onEnd: function(event) {
+        // event.item.style.filter = "";
+        event.item.style.color = "";
+        event.item.style.filter = "brightness(1) blur(0)";
+    }
+});
+
+const botMessages = document.querySelector("#bot-messages");
+
+new Sortable(botMessages, {
+    animation: 150,
+    ghostClass: 'blue-background-class',
+    onChange: function () {
+        let x = 0;
+        botMessages.querySelectorAll(".text-inputs").forEach(inputDiv => {
+            inputDiv.querySelectorAll("input[type=text]").forEach(input => {
+                input.name = input.getAttribute('name').replace(/\[(\d+)\]/, '[' + x + ']');
+            })
+            x++;
+        })
+
+        x = 0;
+        botMessages.querySelectorAll(".text-inputs").forEach(inputDiv => {
+            inputDiv.querySelectorAll("input[type=hidden]").forEach(input => {
+                console.log(input);
+                input.name = input.getAttribute('name').replace(/\[(\d+)\]/, '[' + x + ']');
+            })
+            x++;
+        })
+
+
+
+
     }
 });
